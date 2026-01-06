680f60dfbae8a34e3d92f32bc4e8309d583dde8f
AryanChalpe <chalpearyan@gmail.com>
2026-01-06 18:06:33 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private int age;
+
+    public User() {}
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // getters & setters
+}
