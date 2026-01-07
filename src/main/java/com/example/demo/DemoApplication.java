86dd6c43497754e6032c2518215b369607c20d52
AryanChalpe <chalpearyan@gmail.com>
2026-01-07 18:22:39 +0530

    Updated code with new features

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(title = "Demo API", version = "v1", description = "Demo API endpoints")
+)
 public class DemoApplication {
 
     public static void main(String[] args) {
